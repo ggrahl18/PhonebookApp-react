8443692cf44838f089f391ed78a20dfdfc32681a
boss <ggrahl18@gmail.com>
2020-04-16 15:12:49 -0500

    build added for production

---
+++ build/precache-manifest.58a06308759cffa60682717a00de1d7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed2536f42adda1e85e86e9ff22d4cbe2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5f4166fa2f52936d645",
+    "url": "/static/css/main.9003fb5c.chunk.css"
+  },
+  {
+    "revision": "c8806c721a9ce1bb3bb5",
+    "url": "/static/js/2.1c4526c7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1c4526c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5f4166fa2f52936d645",
+    "url": "/static/js/main.f3e3e5c4.chunk.js"
+  },
+  {
+    "revision": "d19032c1263fa01ea41a",
+    "url": "/static/js/runtime-main.08633c47.js"
+  }
+]);
--- index.js
+++ index.js
@@ -3,8 +3,9 @@ var morgan = require('morgan')
 const app = express()
 const cors = require('cors')
 
-app.use(cors())
-app.use(express.json())
+// app.use(cors())
+// app.use(express.json())
+app.use(express.static('build'))
 
 app.use(
     morgan(
